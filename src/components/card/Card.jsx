b494c4ea07a2eae8003a3509135a5db20f5a90ea
Glazunova Tomara <glazunovatomara5@gmail.com>
2025-01-17 03:00:47 +0700

    feat:add propTypes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,27 @@
 import styles from'./card.module.css';
+import propTypes from 'prop-types';
 
 function Card(props) {
+    const {title, price, info, traffic, color} = props;
+
+    
 
     return(
         <div className={styles.card}>
-            <h2 className={styles.title}>{props.title}</h2>
-            <div className={styles.card_price}><span className={styles.currency}>руб</span> <span className={styles.price}>{props.price}</span>/мес</div>
-            <p className={styles.info}>{props.info}</p>
-            <p className={styles.traffic}>{props.traffic}</p>
+            <h2 className={styles.title}>{title}</h2>
+            <div className={styles.card_price}><span className={styles.currency}>руб</span> <span className={styles.price}>{price}</span>/мес</div>
+            <p className={styles.info}>{info}</p>
+            <p className={styles.traffic}>{traffic}</p>
         </div>
-    )
+    );
+};
 
-}
+Card.propTypes= {
+    title: propTypes.string,
+    price: propTypes.string,
+    info: propTypes.string,
+    traffic: propTypes.string,
+    color: propTypes.string,
+};
 
 export default Card;
--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -9,24 +9,28 @@ function InfoList() {
                 price="300"
                 info="до 10 Мбит/сек"
                 traffic="Oбъем включенного трафика не ограничен"
+                color= "blue"
             />
             <Card 
                 title="безлимитный 450"
                 price="450"
                 info="до 50 Мбит/сек"
                 traffic="Oбъем включенного трафика не ограничен"
+                color= "green"
             />
             <Card 
                 title="безлимитный 550"
                 price="550"
                 info="до 100 Мбит/сек"
                 traffic="Oбъем включенного трафика не ограничен"
+                color= "red"
             />
             <Card 
                 title="безлимитный 1000"
                 price="1000"
                 info="до 200 Мбит/сек"
                 traffic="Oбъем включенного трафика не ограничен"
+                color= "black"
             />
         </React.Fragment>
     )
